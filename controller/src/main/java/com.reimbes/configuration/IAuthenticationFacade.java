d5c0e8e188149b9434a2f38405d176084c733184
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-04-18 11:45:08 +0700

    add authentication helper method

---
+++ controller/src/main/java/com.reimbes/configuration/IAuthenticationFacade.java
@@ -0,0 +1,7 @@
+package com.reimbes.configuration;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthentication();
+}
--- controller/src/main/java/com.reimbes/configuration/RestAuthenticationEntryPoint.java
+++ controller/src/main/java/com.reimbes/configuration/RestAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.reimbes.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
