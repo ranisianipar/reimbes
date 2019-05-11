2540785455d8d98318357e77295e40dab3da3d2a
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-05-11 23:39:46 +0700

    try to switch ocr service, the free one hehe

--- service/src/main/java/com/reimbes/implementation/TesseractService.java
+++ service/src/main/java/com/reimbes/implementation/TesseractService.java
@@ -2,12 +2,27 @@ package com.reimbes.implementation;
 
 import com.reimbes.OcrService;
 import net.sourceforge.tess4j.Tesseract;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@Service
 public class TesseractService implements OcrService {
-    Tesseract instance = new Tesseract();
+    private final Tesseract instance = new Tesseract();
 
     public Tesseract getInstance() {
         instance.setLanguage("en");
         return instance;
+    }
+
+    public String readImage(String imagePath) throws IOException {
+        BufferedImage image = ImageIO.read(new File(StringUtils.cleanPath(imagePath)));
+
+//        instance.doOCR();
+        return "";
     }
 }
