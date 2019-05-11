368fcf49c400d8b401c5cc47c47b6d915f35897a
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-05-11 17:00:31 +0700

    revise the api spec

--- webmodel/src/main/java/com/reimbes/response/TransactionResponse.java
+++ webmodel/src/main/java/com/reimbes/response/TransactionResponse.java
@@ -8,13 +8,11 @@ import java.util.Date;
 @Data
 public class TransactionResponse<T> {
     private long id;
+
     private String image;
 
     private Date created_at;
 
-    // OCR results
-    private Date date;
-    private long amount;
     private Transaction.Category category;
 
     private T transactionDetails;
