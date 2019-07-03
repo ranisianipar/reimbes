66bb9a7739ef9c40303240f9a9e38c9ff5fe9697
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-07-03 23:24:47 +0700

    fix admin parameter name on url path

--- service/src/main/java/com/reimbes/AdminService.java
+++ service/src/main/java/com/reimbes/AdminService.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AdminService {
-    Page getAllUser(String search, Pageable pageable);
+    Page getAllUser(String search, Pageable pageable) throws ReimsException;
     ReimsUser getUser(long id) throws ReimsException;
     ReimsUser createUser(ReimsUser user) throws Exception;
     ReimsUser updateUser(long id, ReimsUser user) throws Exception;
