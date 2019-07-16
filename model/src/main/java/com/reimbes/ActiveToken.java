722c76ccb9f051fe6b25b30fc50d43fde1d704e9
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-07-16 18:11:20 +0700

    postgre ready!

--- model/src/main/java/com/reimbes/ActiveToken.java
+++ model/src/main/java/com/reimbes/ActiveToken.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class ActiveToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     private String token;
