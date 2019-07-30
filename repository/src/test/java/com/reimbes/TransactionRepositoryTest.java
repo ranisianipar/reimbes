b74b6d507d08c28d1bf015c8ffe6fc855dfc9af8
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-07-31 00:16:32 +0700

    test error

--- repository/src/test/java/com/reimbes/TransactionRepositoryTest.java
+++ repository/src/test/java/com/reimbes/TransactionRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.reimbes;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,10 +23,51 @@ public class TransactionRepositoryTest {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    // need to mock userRepository
 
+    private Fuel fuel;
+    private Parking parking;
+    private ReimsUser user;
 
     @Before
     public void setup() {
+        user = new ReimsUser();
+        user.setId(123);
+        entityManager.persistAndFlush(user);
 
+
+        fuel = new Fuel();
+        fuel.setTitle("The fuel");
+        fuel.setCategory(Transaction.Category.FUEL);
+        fuel.setImage(user.getId()+"/abc.jpg");
+        fuel.setType(Fuel.Type.PERTALITE);
+        fuel.setLiters(15);
+        fuel.setDate(1);
+        fuel.setReimsUser(user);
+        fuel.setAmount(150000);
+
+        parking = new Parking();
+        parking.setTitle("The parking");
+        parking.setCategory(Transaction.Category.PARKING);
+        parking.setImage(user.getId()+"/xyz.jpg");
+        parking.setType(Parking.Type.CAR);
+        parking.setLocation("Thamrin");
+        parking.setLicense("K JKSZ PJ");
+        parking.setHours(4);
+        parking.setAmount(21000);
+
+        entityManager.persistAndFlush(fuel);
+        entityManager.persistAndFlush(parking);
     }
+
+    @Test
+    public void checkTransactionExistanceByImage() {
+        assertTrue(transactionRepository.existsByImage(fuel.getImage()));
+        assertTrue(transactionRepository.existsByImage(parking.getImage()));
+
+        assertFalse(transactionRepository.existsByImage("123/hey.jpg"));
+    }
+
+
+
 }
