2c05f104327612963a2204862b4c2391dd4cc07d
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-07-30 18:39:31 +0700

    add get image method in transaction controller

--- controller/src/main/java/com.reimbes/TransactionController.java
+++ controller/src/main/java/com.reimbes/TransactionController.java
@@ -79,6 +79,11 @@ public class TransactionController {
         return br;
     }
 
+    @GetMapping(UrlConstants.IMAGE_PARAM)
+    public byte[] getImage(@PathVariable String image) {
+        return transactionService.getImage(image);
+    }
+
     @PutMapping
     public BaseResponse<TransactionResponse> updateTransaction(@RequestBody TransactionRequest newTransaction) {
         BaseResponse br = new BaseResponse();
