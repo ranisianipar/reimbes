1d76e2d3cf78c035a05f8a6a772ef6dc5fbecd8c
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-08-06 18:15:31 +0700

    done the create user method test, found error when testing update user with some null fields

--- service/src/test/java/com/reimbes/UserServiceTest.java
+++ service/src/test/java/com/reimbes/UserServiceTest.java
@@ -22,6 +22,7 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.gen5.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -80,13 +81,64 @@ public class UserServiceTest {
     }
 
     @Test
-    public void returnUpdatedUserData_whenUpdateUser() throws ReimsException{
+    public void returnUpdatedUserData_whenAdminUpdateUser() throws ReimsException{
         when(passwordEncoder.encode(user.getPassword())).thenReturn(userWithEncodedPass.getPassword());
         when(userRepository.save(user)).thenReturn(userWithEncodedPass);
 
         ReimsUser newUser = userService.create(user);
 
+        when(userRepository.findOne(newUser.getId())).thenReturn(newUser);
 
+        String oldUsername = newUser.getUsername();
+        newUser.setUsername(newUser.getUsername()+"123");
+
+        when(userRepository.findByUsername(newUser.getUsername())).thenReturn(newUser);
+        when(userRepository.save(newUser)).thenReturn(newUser);
+
+        newUser = userService.update(newUser.getId(), newUser);
+
+        assertNotEquals(newUser.getUsername(), oldUsername);
+        assertNotNull(newUser.getUpdatedAt());
+    }
+
+    @Test
+    public void returnUpdatedUserData_whenUserUpdateTheirOwnData() throws ReimsException {
+        when(passwordEncoder.encode(user.getPassword())).thenReturn(userWithEncodedPass.getPassword());
+        when(userRepository.save(user)).thenReturn(userWithEncodedPass);
+        ReimsUser newUser = userService.create(user);
+
+        when(authService.getCurrentUsername()).thenReturn(newUser.getUsername());
+        when(userRepository.findByUsername(newUser.getUsername())).thenReturn(newUser);
+
+        String oldUsername = newUser.getUsername();
+        newUser.setUsername(newUser.getUsername()+"123");
+
+        when(userRepository.findByUsername(newUser.getUsername())).thenReturn(newUser);
+        when(userRepository.save(newUser)).thenReturn(newUser);
+
+        newUser = userService.update(0, newUser);
+
+        assertNotEquals(newUser.getUsername(), oldUsername);
+        assertNotNull(newUser.getUpdatedAt());
+    }
+
+    @Test
+    public void expectedErrorThrown_whenUserUpdateDataWithSomeNullFieldsData() throws ReimsException {
+
+        when(passwordEncoder.encode(user.getPassword())).thenReturn(userWithEncodedPass.getPassword());
+        when(userRepository.save(user)).thenReturn(userWithEncodedPass);
+
+        ReimsUser newUser = userService.create(user);
+
+        when(userRepository.findOne(newUser.getId())).thenReturn(newUser);
+
+        newUser.setUsername(null);
+        newUser.setPassword(null);
+
+
+        assertThrows(ReimsException.class, () -> {
+            userService.update(newUser.getId(), newUser);
+        });
     }
 
 
