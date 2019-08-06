394d9bd7f558c41a60f1169b0c9ead1018ca17f1
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-08-07 00:56:13 +0700

    convert image to string when asked for an image

--- service/src/main/java/com/reimbes/implementation/TransactionServiceImpl.java
+++ service/src/main/java/com/reimbes/implementation/TransactionServiceImpl.java
@@ -259,14 +259,14 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     @Override
-    public byte[] getImage(long id, String imageName) throws ReimsException {
+    public String getImage(long id, String imageName) throws ReimsException {
         if (id != userService.getUserByUsername(authService.getCurrentUsername()).getId())
             throw new NotFoundException("IMAGE");
 
         String imagePath = id+"/"+imageName;
         imagePath = StringUtils.cleanPath(UrlConstants.IMAGE_FOLDER_PATH + imagePath);
         try {
-            return Base64.getEncoder().encode(Files.readAllBytes(Paths.get(imagePath)));
+            return new String(Base64.getEncoder().encode(Files.readAllBytes(Paths.get(imagePath))));
         } catch (Exception e) {
             e.printStackTrace();
         }
