0c2a5427a47dfcbbd4504e8e3d3f3ca28881a738
Rani Lasma Uli <rani.sianipar@gmail.com>
2020-01-23 11:31:33 +0700

    add api test

--- controller/src/test/java/com/reimbes/AuthControllerTest.java
+++ controller/src/test/java/com/reimbes/AuthControllerTest.java
@@ -1,5 +1,7 @@
 package com.reimbes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reimbes.response.UserResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.reimbes.constant.SecurityConstants.HEADER_STRING;
+import static com.reimbes.constant.SecurityConstants.TOKEN_PREFIX;
 import static com.reimbes.constant.UrlConstants.API_PREFIX;
 import static com.reimbes.constant.UrlConstants.LOGIN_URL;
 import static junit.framework.TestCase.assertTrue;
@@ -20,8 +24,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //public class HttpRequestTest {
@@ -43,28 +46,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ComponentScan(basePackages = {"com.reimbes"})
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HttpRequestTest {
+public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ReimsUserRepository userRepository;
+
     @Test
     public void doLogin() throws Exception {
+        ReimsUser userLoginRequest = ReimsUser.ReimsUserBuilder()
+                .username("chrisevan").password("chrisevan123").build();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ReimsUser realUser = userRepository.findByUsername(userLoginRequest.getUsername());
+        UserResponse expectedResult = new UserResponse();
+        expectedResult.setUsername(realUser.getUsername());
+        expectedResult.setRole(realUser.getRole());
+        expectedResult.setId(realUser.getId());
+
         this.mockMvc.perform(post(API_PREFIX + LOGIN_URL)
-                .param("username", "chrisevan")
-                .param("password", "chrisevan123")
-        )
+                .content(mapper.writeValueAsString(userLoginRequest)))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
+                .andExpect(header().string(HEADER_STRING, containsString(TOKEN_PREFIX)))
+                .andExpect(content().string(mapper.writeValueAsString(expectedResult)));
     }
 
-    @Test
-    public void doTest() throws Exception {
-        
-
-        this.mockMvc.perform(get("/api/test").contentType(APPLICATION_JSON_UTF8)
-                .content(""))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
-    }
+//    @Test
+//    public void doTest() throws Exception {
+//
+//
+//        this.mockMvc.perform(get("/api/test").contentType(APPLICATION_JSON_UTF8)
+//                .content(""))
+//                .andDo(print()).andExpect(status().isOk())
+//                .andExpect(content().string(containsString("Hello, World")));
+//    }
 }
